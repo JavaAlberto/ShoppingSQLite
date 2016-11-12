edeeed2c422ab60773bee2ce55b99fed259df543
Erich Schmid <JavaAlberto@freenet.de>
2016-11-12 19:28:30 +0100

    unnötiger Zwischencommit

--- app/src/main/java/berufsschulefreising/de/shoppingsqlite/MainActivity.java
+++ app/src/main/java/berufsschulefreising/de/shoppingsqlite/MainActivity.java
@@ -3,6 +3,8 @@ package berufsschulefreising.de.shoppingsqlite;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -53,6 +55,26 @@ public class MainActivity extends AppCompatActivity {
         ListView shoppingListView = (ListView) findViewById(R.id.listview_shopping);
         shoppingListView.setAdapter(shoppingArrayAdapter);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
